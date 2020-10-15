2df884802bd276f79bab6949f520f922b86b8e27
Revin Tanri <tanrirevin@gmail.com>
2020-10-16 00:07:21 +0800

    Add toLocaleDateString into the TodoList title

--- app.js
+++ app.js
@@ -14,35 +14,15 @@ app.set('view engine', 'ejs');
 //simple get route that will send user "hello" when user try to access home route
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+ 
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    // checking what day of the week
-    switch (currentDay){
-        case 0:
-            day = "Sunday";
-        break;
-        case 1:
-            day = "Monday";
-        break;
-        case 2:
-            day = "Tuesday";
-        break;
-        case 3:
-            day = "Wednesday";
-        break;
-        case 4:
-            day = "Thursday";
-        break;
-        case 5:
-            day = "Friday";
-        break;
-        case 6:
-            day = "Saturday";
-        break;
-        default:
-            console.log("Error: Current day is equal to: " + currentDay);
-    }
+    var day = today.toLocaleDateString("en-US", options);
+
 
     // you need to create VIEW folder with List.ejs, you pass over the logic into the Template file
     res.render("list", {kindOfDay: day});
